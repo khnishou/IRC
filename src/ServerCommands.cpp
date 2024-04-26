18de0ca1bc9f412075f48837e0887a5936caa241
ykerdel <kerdelyounes@gmail.com>
2024-04-26 14:05:07 +0200

    fix: c_mode

--- src/ServerCommands.cpp
+++ src/ServerCommands.cpp
@@ -77,17 +77,19 @@ void	Server::c_mode(std::vector<std::string> param, Users *user)
 
 	// need 2 revisit this logic, if its unsetting a mode than no params must be provided, if trying to set then need params
 	if (!(param.size() >= 1))
-		return (user->setBuffer(ERR_NEEDMOREPARAMS(user->getNickName(), "MODE"))); //(461) 
+		return (user->setBuffer(ERR_NEEDMOREPARAMS(user->getNickName(), "MODE"))); //(461)
 	i = 0;
 	Channel *channel = getChannel(param[0]);
 	if (!channel)
 		return (user->setBuffer(ERR_NOSUCHCHANNEL(this->host, user->getNickName(), param[0]))); // 403)
+	if (param.size() < 2)
+		return ; // RPL_CHANNELMODEIS (324)
 	mode = initMode(param, mode, channel);
 	if (mode & (1 << 7))
 		return (user->setBuffer(ERR_UMODEUNKNOWNFLAG(this->host, user->getNickName()))); //(501)
-	if (!channel->isUser(user))
+	if (!channel->isUser(user) && !channel->isOperator(user))
 		return (user->setBuffer(ERR_NOTONCHANNEL(this->host, user->getNickName(), channel->getName()))); // (442)
-	if (!channel->isOperator(user))
+	if (!channel->isOperator(user)) // check extra priv
 		return (user->setBuffer(ERR_CHANOPRIVSNEEDED(this->host, user->getNickName(), channel->getName()))); // (482)
 	channel->setMode(mode);
 	// success rpl value basically sends a message on the channel to every user saying what the new modes are
@@ -196,22 +198,22 @@ uint8_t Server::initMode(std::vector<std::string> param, uint8_t mode, Channel *
 	uint8_t	setUnset;
 	uint8_t	flag;
 
-	itParam = 0;
+	itParam = 1;
 	setUnset = 0;
 	flag = 0;
 	i = 0;
-	len = param[0].length();
-	while (i < param[0].length())
+	len = param[1].length();
+	while (i < param[1].length())
 	{
-		if (param[0][i] == '+')
+		if (param[1][i] == '+')
 			setUnset = FLAG_SET;
-		else if (param[0][i] == '-')
+		else if (param[1][i] == '-')
 			setUnset = FLAG_UNSET;
-		else if (param[0][i] == 'i')
+		else if (param[1][i] == 'i') // type D
 			mode = setTheUnset(mode, FLAG_I, setUnset);
-		else if (param[0][i] == 't')
+		else if (param[1][i] == 't') // type D
 			mode = setTheUnset(mode, FLAG_T, setUnset);
-		else if (param[0][i] == 'k')
+		else if (param[1][i] == 'k')
 		{
 			if (setUnset & FLAG_SET)
 			{
@@ -222,7 +224,7 @@ uint8_t Server::initMode(std::vector<std::string> param, uint8_t mode, Channel *
 			}
 			mode = setTheUnset(mode, FLAG_K, setUnset);
 		}
-		else if (param[0][i] == 'o')
+		else if (param[1][i] == 'o')
 		{
 			if (param[++itParam].empty())
 				; // error need more param or do nothing
@@ -231,11 +233,11 @@ uint8_t Server::initMode(std::vector<std::string> param, uint8_t mode, Channel *
 				if (setUnset & FLAG_SET)
 					channel->addOperator(getUserByUn(param[itParam]));
 				else if (setUnset & FLAG_UNSET)
-					;// check channel->deleteOperator(getUserByUn(param[itParam]));
+					; // check channel->deleteOperator(getUserByUn(param[itParam]));
 			}
 			// check mode = setTheUnset(mode, FLAG_O, setUnset); // we don t need to set/unset it anymore but still there
 		}
-		else if (param[0][i] == 'l')
+		else if (param[1][i] == 'l')
 		{
 			if (setUnset & FLAG_SET)
 			{
