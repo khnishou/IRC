60d2d04904b7c2141ac1b3578a8cffa9a57b110f
ykerdel <kerdelyounes@gmail.com>
2024-04-30 21:42:45 +0200

    feat: add PART cmd

--- src/ServerCommands.cpp
+++ src/ServerCommands.cpp
@@ -14,7 +14,19 @@ void	Server::c_cap(std::vector<std::string> param, Users *user) {
 }
 
 void	Server::c_part(std::vector<std::string> param, Users *user) {
-
+	if (param.size() < 1)
+		return (user->setBuffer(ERR_NEEDMOREPARAMS(user->getNickName(), "PART"))); // (461)
+	std::vector<std::string> split = splitString(param[0], ',');
+	for (std::vector<std::string>::iterator it = split.begin(); it != split.end(); ++it)
+	{
+		Channel *channel = getChannel(*it);
+		if (!channel)
+			return (user->setBuffer(ERR_NOSUCHCHANNEL(this->host, user->getNickName(), *it))); // (403)
+		if (!channel->isUser(user) && !channel->isOperator(user))
+			return (user->setBuffer(ERR_NOTONCHANNEL(this->host, user->getNickName(), channel->getName()))); // (442)
+		// check leaving a channel
+		// check RPL leaving a channel
+	}
 }
 
 void	Server::c_ping(std::vector<std::string> param, Users *user) {
